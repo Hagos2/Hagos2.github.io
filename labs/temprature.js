2ffc15dab528cdbea53c83846cdcd963d2ce3f84
Gebrecherkos Hagos <gebrecherkoshagos@Gebrecherkoss-MacBook-Pro.local>
2022-02-22 22:02:38 -0600

    update temprature

--- labs/temprature.js
+++ labs/temprature.js
@@ -1,8 +1,8 @@
 function convertFahrenheit(fahrenheitTemp) {
     return (fahrenheitTemp - 32) * (5 / 9);
   }
-  console.log("expect 0: ", convertFahrenheit(32));
-  console.log("expect -17.7778: ", convertFahrenheit(0));
-  console.log("expect 100: ", convertFahrenheit(212));
-  console.log("expect 37.7778: ", convertFahrenheit(100));
+  console.log("expect 0: ", convertFahrenheit(32).toFixed(0));
+  console.log("expect -17.7778: ", convertFahrenheit(0).toFixed(4));
+  console.log("expect 100: ", convertFahrenheit(212).toFixed(0));
+  console.log("expect 37.7778: ", convertFahrenheit(100).toFixed(4));
   
