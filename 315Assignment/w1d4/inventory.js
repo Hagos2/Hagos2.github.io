725057f29c8cc7c61abdb5173c93f9b0e6469fcb
Gebrecherkos Hagos <gebrecherkoshagos@Gebrecherkoss-MacBook-Pro.local>
2022-04-02 03:28:56 -0500

    CS315Assignment

--- 315Assignment/w1d4/inventory.js
+++ 315Assignment/w1d4/inventory.js
@@ -1,94 +1,86 @@
+/* eslint-disable*/
+let myArray=[];
+let prodname=document.getElementById("name");
+let category=document.getElementById("category");
+let qty= document.getElementById("quantity");
+let rate= document.getElementById("rate")
+let tabl=document.querySelector("table");
 
-/*eslint-disable*/
-var selectedRow = null;
-let sortDirection = false;
-window.onload = () => {
-  loadData(addList);
-};
-function onFormSubmit() {
-  var formData = readData();
-  if (selectedRow == null) addList(filledData);
-  else updatedRecord(filledData);
-  addList(filledData);
-  resetForm();
-}
-function addList(data) {
-  var table = document
-    .getElementById("itemList")
-    .getElementsByTagName("tbody")[0];
-  var newRow = table.insertRow(table.length);
-  cel1 = newRow.insertCell(0);
-  cel1.innerHTML = data.name;
-  cel2 = newRow.insertCell(1);
-  cel2.innerHTML = data.category;
-  cel3 = newRow.insertCell(2);
-  cel3.innerHTML = data.quantity;
-  cel4 = newRow.insertCell(3);
-  cel4.innerHTML = data.ratings;
-  cel4 = newRow.insertCell(4);
-  cel4.innerHTML = `<a onclick="onEdit(this)">Edit</a>
-                          <a>Delete</a>`;
-}
-function submit() {
-  var filledData = read();
-  addRow(filledData);
-}
-function readData() {
-  var filledData = [];
-  filledData["name"] = document.getElementById("name").value;
-  filledData["category"] = document.getElementById("category").value;
-  filledData["quantity"] = document.getElementById("quantity").value;
-  filledData["ratings"] = document.getElementById("ratings").value;
 
-  return filledData;
+function add(){
+
+   let empty=prodname.value ==="" || category.value ==="" || qty.value ==="" || rate.value==="";
+
+    if(isNaN(+qty.value)){ 
+       
+        alert("quantity must be a number!!");
+        qty.style.background="blue";
+    }else if(empty){ 
+        alert("No field should be empty !!!");  
+    }else{
+
+    let tr= document.createElement("tr");
+
+    let bb=document.createAttribute("button")
+    tabl.append(tr);
+
+    tr.innerHTML=" <td> " + prodname.value + "</td> <td> "+ category.value + "</td> <td> "+
+      qty.value + "</td> <td> "+  rate.value + "</td> <td> <button  "+"onclick= "+"edit(this)"+
+      " "+"   >"+"Edit"+"</button></td> <td><button  "+"onclick= "+"remove(this)"+" "+"    >"+"Remove"+"</button></td>"
+
+   
+let obj={name:prodname.value, category:category.value, qty:qty.value,rate:rate.value }
+console.log(obj);
+myArray.push(obj);
+
+ 
+    prodname.value = "";
+    category.value="";
+    qty.value="";
+    rate.value="";
+    qty.style.background="";
+
+
 }
-function resetForm() {
-  document.getElementById("name").value = "";
-  document.getElementById("category").value = "";
-  document.getElementById("quantity").value = "";
-  document.getElementById("ratings").value = "";
-  selectedRow = null;
-}
-function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("name").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("category").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("quantity").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("ratings").value = selectedRow.cells[3].innerHTML;
-}
-function updatedRecord(filledData) {
-  selectedRow.cells[0].innerHTML = filledData.name;
-  selectedRow.cells[1].innerHTML = filledData.category;
-  selectedRow.cells[2].innerHTML = filledData.quantity;
-  selectedRow.cells[3].innerHTML = filledData.ratings;
-}
-function onDelete(td) {
-  if (confirm("Are you sure you want to delete this data?")) {
-    row = td.parentElement.parentElement;
-    document.getElementById("itemList").deleteRow(row, rowIndex);
-  }
-}
-function filter(addList) {
-  addList().filter((a) => a.quantity > 100);
+}   
+    
+console.log(myArray);
+
+function edit(obj){
+
+   let row=obj.parentNode.parentNode.children;
+
+   prodname.value = row[0].innerText
+   category.value = row[1].innerText
+   qty.value = row[2].innerText
+   rate.value = row[3].innerText  
+
+remove(obj);
 }
 
-function sort(column) {
-  const dataType = typeof addList[0](column);
-  sortDirection = !sortDirection;
-  switch (dataType) {
-    case number:
-      sortCol(sortDirection, column);
-      break;
-    case string:
-      sortCol(sortDirection, column);
-      break;
-  }
-  loadData(addList);
+let save= document.getElementById("save")
+save.addEventListener("onclick", add);
+
+function remove(obj){
+ 
+    let row= obj.parentNode.parentNode;
+  
+    row.remove();
+    let rowindex= obj.parentNode.parentNode.rowIndex;
+    myArray.splice(rowindex, 1)
+    console.log(myArray)
+    }
+
+// The selection part.
+
+let filter= document.getElementById("filter");
+let sort= document.getElementById("sort");
+
+filter.addEventListener("click", filt);
+
+function filt(){
+
+
 }
 
-function sortCol(sort, column) {
-  let enteredData = addList();
-  enteredData = enteredData.sort((a, b) => {
-    return sort ? a[column] - b[column] : b[column] - a[column];
-  });
-}
+  
