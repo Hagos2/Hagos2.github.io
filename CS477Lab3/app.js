b2903abc3d5091848b85620cbadcde87325920c0
Gebrecherkos Hagos <gebrecherkoshagos@Gebrecherkoss-MacBook-Pro.local>
2022-07-24 18:55:13 -0500

    CS477Lab3

--- CS477Lab3/app.js
+++ CS477Lab3/app.js
@@ -1,48 +1,93 @@
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer((req, res) => {
+//     console.log(req.url);
+//     const url = req.url;
+
+
+//     if (url === '/') {
+//         fs.createReadStream('index.html').pipe(res);
+//     } else if (url === '/signup' && req.method === 'POST') {
+//         const body = [];
+//         req.on('data', (chunk) => {
+//             body.push(chunk);
+//         });
+
+//         req.on('end', () => {
+//             const postData = Buffer.concat(body).toString();
+//             console.log(postData);
+//             const postDataArr = postData.split('&'); //[firstname=john, lastname=Doe]
+//             const firstNameArr = postDataArr[0].split('='); // [firstname, john]
+//             const lastnameArr = postDataArr[1].split('='); //[lastname, Doe]
+
+//             if (firstNameArr[0] === 'firstname' && firstNameArr[1] === 'john' && lastnameArr[0] === 'lastname' && lastnameArr[1] === 'smith') {
+//                 res.end(`${firstNameArr[1]} ${lastnameArr[1]} already exists! Please try another one!`);
+//             } else {
+//                 res.end('Signup Successfully');
+//             }
+//         });
+//     } else {
+//         res.writeHead(404);
+//         res.end('404 Page Not Found');
+//     }
+
+
+
+// }).listen(3000, () => console.log('listening to 3000'));
+
+
+const http = require('http');
 const fs = require('fs');
 
-// var options = {
-//     key: fs.readFileSync('./privateKey.key'),
-//     cert: fs.readFileSync('./certificate.crt')
-// };
-
-const server = require('https')
-    //.createServer(options);
-
-server.on('request', (req, res) => {
-    // console.log(req.url, req.method, req.headers);
+http.createServer((req, res)=>{
+    console.log(req.url);
     const url = req.url;
-    const method = req.method;
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/messsage" method="POST">Enter Message: <input name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end(); // "retrun" here exits the function execution, otherwise continue.
-    }
-
-    if (url === '/messsage' && method === 'POST') {
+    if(url === '/') {
+        fs.createReadStream('index.html').pipe(res);
+    }else if(url === '/success' && req.method === 'POST'){
         const body = [];
-        req.on('data', (chunk) => {
+        req.on('data', (chunk)=>{
             body.push(chunk);
         });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            fs.writeFileSync('message.txt', parsedBody.split('=')[1]);
+
+        req.on('end', ()=>{
+            const postData = Buffer.concat(body).toString();
+            console.log(postData);
+            const postDataArr = postData.split('&'); 
+            const firstNameArr = postDataArr[0].split('='); 
+            const lastnameArr = postDataArr[1].split('='); 
+            fs.writeFile('message.txt', `${firstNameArr[1]}=${lastnameArr[1]}`, (error)=>{
+                if(error) res.end('Try later...');
+                else res.end('Sign up successfully');
+            });
+           
         });
+    } 
 
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-    }
+}).listen(3000, ()=> console.log('listening to 3000'));
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hellow from Node.js</h1></body>');
-    res.write('</html>');
-    res.end();
-});
 
-server.listen(443);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
